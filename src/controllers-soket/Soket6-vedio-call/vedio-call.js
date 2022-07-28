2217a97061c037dcea2be9e7c562f88ab8d060cc
lithhalim <103363189+lithhalim@users.noreply.github.com>
2022-07-28 20:15:49 +0300

    d

---
+++ src/controllers-soket/Soket6-vedio-call/vedio-call.js
@@ -0,0 +1,36 @@
+module.exports=(socket,io)=>{
+    socket.on("vedioCallRoom",(data)=>{
+        socket.join(data.regusterid)
+    })
+
+    //get request from user want to create vedio call
+    socket.on("requestPeerId",(data)=>{
+        //send the data for the another clinet 
+        socket.to(data.accept_Request_Id).emit("get-vedioCall-request",data)
+    })
+
+    //the answer for the another clinet 
+    socket.on("answer-the-request-vedio-call",(data)=>{
+        //the answer send person do the request
+        socket.to(data.Send_Request_Id).emit("accept-status-reject-or-accept",data)
+    })
+
+
+    socket.emit("soketio-id", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+        socket.broadcast.emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+
+
+
+    
+}
