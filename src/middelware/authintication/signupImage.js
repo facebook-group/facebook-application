f0ba2704625e43a174f4e6899f5c85bb938ded40
ahmadijmail <ahmadijmil2010@gmail.com>
2022-07-30 08:01:53 +0300

    add signupImage

---
+++ src/middelware/authintication/signupImage.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+
+
+
+ storage = new multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
+});
+
+
+  upload= multer({ storage: storage }).single("image");
+
+
+
+
+
+module.exports=upload
