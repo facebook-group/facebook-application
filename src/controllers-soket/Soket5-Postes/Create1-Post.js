d7f0004ecb8e76bc2ed1ec24740665fa2f624a02
Qais Almanasra <qmanasra@gmail.com>
2022-08-01 10:46:20 +0300

    modyfy socket

--- src/controllers-soket/Soket5-Postes/Create1-Post.js
+++ src/controllers-soket/Soket5-Postes/Create1-Post.js
@@ -1,7 +1,7 @@
 
 let Post_model=require("../../model/postes-model/post-model");
 
-
+// prepare the post and create it
 module.exports=(socket,io)=>{
     socket.on("create_postes",async(data)=>{
         const createPost=await Post_model.create(data);
