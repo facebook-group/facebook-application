60ab32ec9f5180f0bdd3145452ecf3043410376d
ahmadijmail <ahmadijmil2010@gmail.com>
2022-08-02 23:37:51 +0300

    Video call

--- src/controllers-soket/Soket6-vedio-call/vedio-call.js
+++ src/controllers-soket/Soket6-vedio-call/vedio-call.js
@@ -1,36 +1,21 @@
-module.exports=(socket,io)=>{
-    socket.on("vedioCallRoom",(data)=>{
-        socket.join(data.regusterid)
-    })
+module.exports = (socket, io) => {
+  //to every clinet join to room some with his id
+  socket.on("vedio-call-personid-room", (data) => {
+    socket.join(data.regusterid);
+  });
 
-    //get request from any user want to create vedio call
-    socket.on("requestPeerId",(data)=>{
-        //send the data for another clinet 
-        socket.to(data.accept_Request_Id).emit("get-vedioCall-request",data)
-    })
+  //get request from user want to create vedio call
+  socket.on("Vedio-Call-Request", (data) => {
+    //send the data for the another clinet
+    socket.to(data.accept_Request_Id).emit("get-vedioCall-request", data);
+  });
 
-    //the answer for the another clinet 
-    socket.on("answer-the-request-vedio-call",(data)=>{
-        //the answer send person do the request
-        socket.to(data.Send_Request_Id).emit("accept-status-reject-or-accept",data)
-    })
+  socket.on("join-chat-room", (data) => {
+    socket.join(data.chatId);
+  });
 
-
-    socket.emit("soketio-id", socket.id)
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	})
-
-	socket.on("callUser", (data) => {
-        socket.broadcast.emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-	})
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	})
-
-
-
-    
-}
+  //-------------------------get peer id for accepter and sender -----------------------------------//
+  socket.on("peerid-accepter", (data) => {
+    socket.to(data.chatId).emit("get-peerid-accepter", data);
+  });
+};
