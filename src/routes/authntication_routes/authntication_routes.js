3f8745395e12aef44cea43438f67a9ece93da1dd
ahmadijmail <ahmadijmil2010@gmail.com>
2022-08-03 07:40:42 +0300

    fix some routes

--- src/routes/authntication_routes/authntication_routes.js
+++ src/routes/authntication_routes/authntication_routes.js
@@ -1,30 +1,46 @@
 const express = require('express')
 const router = express.Router()
 
-
-//ALL MEDILWARE FUNCTION ARE USE
-const signin_controllers=require("../../controllers/authntication-controllers/signin-controllers");
-const signup_controllers=require("../../controllers/authntication-controllers/signup-controllers");
-const refresh_token=require("../../controllers/authntication-controllers/refresh-token-controllers")
-
-const signout_controllers=require("../../controllers/authntication-controllers/logout-controllers")
-
 //BASIQ AUTH REQUIRE
 const basic_auth=require("../../middelware/authintication/basic_auth")
 const bearer_auth=require("../../middelware/authintication/barear_auth")
 
-const upload=require("../../middelware/authintication/signupImage")
 
-const getAllData_user_controllers=require("../../controllers/authntication-controllers/getalldata-user-controlles")
+
+//ALL MEDILWARE FUNCTION ARE USE
+const signin_controllers=require("../../controllers/authntication-controllers/signin-signout-signup/signin-controllers");
+const signup_controllers=require("../../controllers/authntication-controllers/signin-signout-signup/signup-controllers");
+const signout_controllers=require("../../controllers/authntication-controllers/signin-signout-signup/logout-controllers")
+
 //ALL ROUTES ARE USED
 router.post("/signin",basic_auth,signin_controllers)
-router.post("/signup",upload, signup_controllers);
+router.post("/signup",signup_controllers);
 router.post("/signout",signout_controllers)
 
-//get all information for the user
-router.get("/user/:data",getAllData_user_controllers);
+
+
 //create The Refresh Token
+const refresh_token=require("../../controllers/authntication-controllers/refresh-token-controllers")
 router.post("/refreshtoken",refresh_token)
+
+
+//get all information for the user
+const getAllData_user_controllers=require("../../controllers/authntication-controllers/getalldata-user-controlles")
+router.get("/user/:data",getAllData_user_controllers);
+
+
+//validation check 
+const ModemailerControllers=require("../../controllers/authntication-controllers/nodemailr-validation/Nodemailer-controllaers")
+const ValidationCheck=require("../../controllers/authntication-controllers/nodemailr-validation/validationCheck")
+router.post('/Nodemailer',ModemailerControllers)
+router.post("/validationcheck",ValidationCheck)
+
+
+
+
+const Get_allupdate_role=require("../../controllers/authntication-controllers/update-data-user/get-all-update-role-controlles")
+router.get("/getallupdaterole",Get_allupdate_role)
+
 
 
 
